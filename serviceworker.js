97573d876fed5a4e94dc9cd493164d234b7b3246
Vincent B <evilznet@gmail.com>
2017-12-28 17:59:55 +0100

    format code

--- serviceworker.js
+++ serviceworker.js
@@ -1,17 +1,18 @@
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.open('mysite').then(function(cache) {
-      return cache.match(event.request).then(function(response) {
-        var fetchPromise = fetch(event.request).then(function(networkResponse) {
-          if(event.request.method === "GET")
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.open('mysite').then(function (cache) {
+            return cache.match(event.request).then(function (response) {
+                var fetchPromise = fetch(event.request).then(function (networkResponse) {
+                    if (event.request.method === "GET") {
+                        cache.put(event.request, networkResponse.clone());
+                    }
+                    return networkResponse;
+                })
+
+
+
+                return response || fetchPromise;
+            })
         })
-
-        
-
-        return response || fetchPromise;
-      })
-    })
-  );
+    );
 });
