71323c7db6da4a8ad51f15f282b64906cd23cd4b
Vincent B <evilznet@gmail.com>
2017-12-28 17:53:09 +0100

    only cache get

--- serviceworker.js
+++ serviceworker.js
@@ -3,6 +3,7 @@ self.addEventListener('fetch', function(event) {
     caches.open('mysite').then(function(cache) {
       return cache.match(event.request).then(function(response) {
         var fetchPromise = fetch(event.request).then(function(networkResponse) {
+          if(event.request.method === "GET")
           cache.put(event.request, networkResponse.clone());
           return networkResponse;
         })
